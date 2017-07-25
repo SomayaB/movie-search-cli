9c239a30762582f2f6ac96f2dc228828fc8b6971
SomayaB <somaya.bounouar@gmail.com>
2017-07-25 14:46:10 -0700

    Add basic query function that returns entire html

--- movie-search.js
+++ movie-search.js
@@ -1,2 +1,22 @@
 const http = require('http')
 const cheerio = require('cheerio')
+
+
+var searchMovie = process.argv.splice(2).join(' ') //string
+
+function queryIMDB(search){
+  http.get({
+    host: 'www.imdb.com',
+    path: `/find?ref_=nv_sr_fn&q=${search}&s=all`
+  }, function callback(res){
+    var html = ''
+    res.on('data', function(chunk){
+      html += chunk}) //????
+    res.on('end', function(){
+      console.log(html)
+    })
+    })
+  }
+
+
+queryIMDB("nemo")

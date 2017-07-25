deae21c56df2b141b6069eaa97a8c4c98f19febb
SomayaB <somaya.bounouar@gmail.com>
2017-07-25 16:05:27 -0700

    Make queryIMDB function fully functional even with multiple search terms

--- movie-search.js
+++ movie-search.js
@@ -2,21 +2,32 @@ const http = require('http')
 const cheerio = require('cheerio')
 
 
-var searchMovie = process.argv.splice(2).join(' ') //string
-
-function queryIMDB(search){
+function queryIMDB(callback){
+  var searchTerms = encodeURIComponent(process.argv.splice(2).join(' '))
   http.get({
     host: 'www.imdb.com',
-    path: `/find?ref_=nv_sr_fn&q=${search}&s=all`
-  }, function callback(res){
-    var html = ''
-    res.on('data', function(chunk){
-      html += chunk}) //????
-    res.on('end', function(){
-      console.log(html)
+    path: `/find?ref_=nv_sr_fn&q=${searchTerms}&s=all`
+    }, function(res){
+      var html = ''
+      res.on('data', function(chunk){ return html += chunk }) //????
+      res.on('end', function(){
+      var movieNames = getMoviesNames(html)
+      callback(null, movieNames)
     })
-    })
+  })
+}
+
+function getMoviesNames(html){
+  var $ = cheerio.load(html)
+  var movieNames = $('.findSection').first().find('.result_text').map(function(index, element){
+    return $(element).text()
+  }).toArray()
+  return movieNames
+}
+
+queryIMDB(function(error, movieNames){
+  if(error){
+    throw error
   }
-
-
-queryIMDB("nemo")
+  console.log(movieNames.join('\n'))
+})
